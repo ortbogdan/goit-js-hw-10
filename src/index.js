3885e92572be533d5c8c28c2bec5a542a694394e
Bogdan <ortbg1988@gmail.com>
2021-11-08 13:44:53 +0200

    [refactor] refactor a code and add a new files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
-
+import {fetchCountries} from './fetchCountries.js'
 const DEBOUNCE_DELAY = 300;
+const listRef = document.querySelector('.country-list');
+const inputRef = document.querySelector('input#search-box');
+
+
+inputRef.addEventListener('input', onInputGetCountry);
+
+function onInputGetCountry(){
+const countries = fetchCountries(inputRef.value)
+console.log(countries)
+return countries;
+ }
+
+function makeMarkup (countries) {
+    if (countries.length > 10) {
+        console.log("Too many matches found. Please enter a more specific name.")
+    }
+    const contryMarkup = countries.map((country) => {return `<li>${country.flags.svg}${country.name.oficial}</li>`}).join('');
+    listRef.insertAdjacentHTML('beforeend', contryMarkup)
+}
+makeMarkup(countries)
