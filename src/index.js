a0d749b84870988c8e72c9b1e57415da015680c0
Bogdan <ortbg1988@gmail.com>
2021-11-08 16:10:24 +0200

    [add] add a code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import './css/styles.css';
 import {fetchCountries} from './fetchCountries.js'
+import debounce from 'lodash.debounce'
 const DEBOUNCE_DELAY = 300;
 const listRef = document.querySelector('.country-list');
 const inputRef = document.querySelector('input#search-box');
 
-
-inputRef.addEventListener('input', onInputGetCountry);
+inputRef.addEventListener('input', debounce(onInputGetCountry, DEBOUNCE_DELAY));
 
 function onInputGetCountry(){
-const countries = fetchCountries(inputRef.value)
-console.log(countries)
-return countries;
+    
+    const value = inputRef.value.trim();
+    fetchCountries(value).then(countries =>{return  makeMarkup(countries)})
+    console.log(countries)
  }
+
+
+ 
 
 function makeMarkup (countries) {
     if (countries.length > 10) {
         console.log("Too many matches found. Please enter a more specific name.")
     }
-    const contryMarkup = countries.map((country) => {return `<li>${country.flags.svg}${country.name.oficial}</li>`}).join('');
+    const contryMarkup = countries.map(country =>`<li class="country-list__item"><svg class="country-list__flag"><use class="country-list__flag" href=${country.flags.svg}></use></svg>${country.name.official}</li>`).join('');
     listRef.insertAdjacentHTML('beforeend', contryMarkup)
 }
-makeMarkup(countries)
